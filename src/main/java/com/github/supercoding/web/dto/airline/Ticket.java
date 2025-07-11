8063307fef4515e8a9dc0d9c62951040117e8ee3
MalRang <ekaloses@naver.com>
2025-07-11 11:35:42 +0900

    2025.07.11(금) Logging 및 문서화(Swagger대신 GroupedOpenApi)

--- src/main/java/com/github/supercoding/web/dto/airline/Ticket.java
+++ src/main/java/com/github/supercoding/web/dto/airline/Ticket.java
@@ -1,31 +1,31 @@
 package com.github.supercoding.web.dto.airline;
+
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.github.supercoding.repository.airlineTicket.AirlineTicket;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.format.DateTimeFormatter;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Getter
 @Setter
 @NoArgsConstructor
+@Schema(description = "항공권 정보")
 public class Ticket {
+
+    @Schema(description = "승객 출발지", example = "서울")
     private String depart;
+
+    @Schema(description = "승객 도착지", example = "파리")
     private String arrival;
+
+    @Schema(description = "항공권 출발시간", example = "2023-05-05 11:00:00")
     private String departureTime;
+
+    @Schema(description = "항공권 귀국시간", example = "2023-05-07 11:00:00")
     private String returnTime;
+
+    @Schema(name = "ticketId", description = "Ticket ID", example = "1")
     private Integer ticketId;
-
-    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    public Ticket(AirlineTicket airlineTicket){
-        this.ticketId = airlineTicket.getTicketId();
-        this.depart = airlineTicket.getDepartureLocation();
-        this.arrival = airlineTicket.getArrivalLocation();
-        this.departureTime = airlineTicket.getDepartureAt().format(formatter);
-        this.returnTime = airlineTicket.getReturnAt().format(formatter);
-
-    }
 }
